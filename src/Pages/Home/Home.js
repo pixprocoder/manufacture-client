a373a7095b6ee70b6be2f36c2ec7928602ca9e79
kabir <makkobir79@gmail.com>
2022-05-24 21:03:58 +0600

    navbar and banner complited

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Banner from "./Banner";
+import "../../Styles/Home.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Banner />
+    </div>
+  );
+};
+
+export default Home;
