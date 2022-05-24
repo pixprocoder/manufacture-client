626f11bdb4543905ad5f77d2bc3b178153d0f066
kabir <makkobir79@gmail.com>
2022-05-24 21:38:44 +0600

    hame page and banner complited

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -2,21 +2,20 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div class="banner-container h-screen text-white">
-      <div class="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src="https://i.ibb.co/BT0WNX2/fashion-g4c32f53c1-640.jpg"
-          class="max-w-sm rounded-lg shadow-2xl"
-          alt=""
-        />
+    <div class="banner-container h-screen">
+      <div class="text-center mb-[100px]">
         <div>
-          <h1 class="text-5xl font-bold">Box Office News!</h1>
-          <p class="py-6">
+          <h1 class="text-5xl font-bold">START YOUR BUSINESS</h1>
+          <p class="py-6 ">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            excepturi exercitationem quasi. In deleniti eaque <br /> aut
+            repudiandae et a id nisi. yes you can do this.
           </p>
-          <button class="btn btn-primary">Get Started</button>
+
+          <button class="btn btn-outline rounded-3xl">
+            {" "}
+            START YOUR JOURNEY
+          </button>
         </div>
       </div>
     </div>
