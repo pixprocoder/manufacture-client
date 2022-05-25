e70a8411887d0e01cc3e2f29823e9e7e52124273
kabir <makkobir79@gmail.com>
2022-05-25 06:23:27 +0600

    route setup complited

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Footer from "./Pages/Shared/Footer";
 import Navbar from "./Pages/Shared/Navbar";
+import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Login/Signup";
+import Contact from "./Pages/Contact/Contact";
+import About from "./Pages/About/About";
 
 function App() {
   return (
@@ -10,8 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<Home />} />
-        <Route path="/contact" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
       <Footer />
     </>
