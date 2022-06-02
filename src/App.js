d4c46c0113a26418291a426e7efb5c5978833a3e
kabir <makkobir79@gmail.com>
2022-06-02 18:16:15 +0600

    some comment removed

--- src/App.js
+++ src/App.js
@@ -62,17 +62,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
-      <ToastContainer
-      // position="top-center"
-      // autoClose={5000}
-      // hideProgressBar={false}
-      // newestOnTop={false}
-      // closeOnClick
-      // rtl={false}
-      // pauseOnFocusLoss
-      // draggable
-      // pauseOnHover
-      ></ToastContainer>
+      <ToastContainer></ToastContainer>
     </>
   );
 }
