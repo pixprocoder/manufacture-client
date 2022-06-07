e9810b28e0d1a9313f566c90da0029abad185faa
kabir <makkobir79@gmail.com>
2022-06-07 23:04:11 +0600

    replace private repo to public repo

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -5,16 +5,14 @@ const Banner = () => {
     <div className="banner-container h-screen">
       <div className="text-center ">
         <div>
-          <h1 className="text-5xl font-bold">START YOUR BUSINESS</h1>
+          <h1 className="text-5xl font-bold">START YOUR BUSINESS WITH US</h1>
           <p className="py-6 ">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque <br /> aut
-            repudiandae et a id nisi. yes you can do this.
+            hello there, Welcome to our website. We are looking for you!
           </p>
 
           <button className="px-8 py-2 rounded-3xl bg-orange-600 hover:bg-orange-800 text-xl text-white font-bold ">
             {" "}
-            START YOUR JOURNEY
+            Explore more
           </button>
         </div>
       </div>
