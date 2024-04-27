35b9888dd3529075fb8592609abdfa38ca339641
Samsul Kobir <makkobir79@gmail.com>
2024-04-27 04:38:57 +0300

    banner carusel removed

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -4,47 +4,10 @@ import banner from "../../images/contactUs.jpg";
 const Banner = () => {
   return (
     <section>
-      <div
-        id="carouselExampleControls"
-        className="carousel slide"
-        data-bs-ride="carousel"
-      >
-        <div className="carousel-inner">
-          <div className="carousel-item active">
+      {/* TODO:  add carousel */}
+      <div >
             <img src={banner} className="d-block w-100" alt="banner1" />
           </div>
-          <div className="carousel-item">
-            <img src={banner} className="d-block w-100" alt="banner2" />
-          </div>
-          <div className="carousel-item">
-            <img src={banner} className="d-block w-100" alt="banner3" />
-          </div>
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleControls"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleControls"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
     </section>
   );
 };
